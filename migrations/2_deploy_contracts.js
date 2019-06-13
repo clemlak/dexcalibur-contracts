8c9dccdfc789da0be4a06814f5cbb07355707dcb
Clement Lakhal <clementlakhal@gmail.com>
2019-06-14 00:52:02 +0200

    Update migrations file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,18 @@
 /* eslint-env node */
 /* global artifacts */
 
+const TestToken = artifacts.require('TestToken');
 const Dexcalibur = artifacts.require('Dexcalibur');
 
-function deployContracts(deployer) {
-  deployer.deploy(Dexcalibur);
+const manaTokenAddress = '0x2a8fd99c19271f4f04b1b7b9c4f7cf264b626edb';
+
+function deployContracts(deployer, network) {
+  if (network === 'development') {
+    deployer.deploy(TestToken)
+      .then(() => deployer.deploy(Dexcalibur, TestToken.address));
+  } else if (network === 'ropsten') {
+    deployer.deploy(Dexcalibur, manaTokenAddress);
+  }
 }
 
 module.exports = deployContracts;
