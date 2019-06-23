5d515143420cd75efde5998ab21793dbc943d190
Clement Lakhal <clementlakhal@gmail.com>
2019-06-24 01:31:38 +0200

    Change concept

--- contracts/Dexcalibur.sol
+++ contracts/Dexcalibur.sol
@@ -1,56 +1,41 @@
 pragma solidity 0.5.7;
 
-import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
 
 
 /**
  * @title An amazing project called Dexcalibur
  * @dev This contract is the base of our project
  */
-contract Dexcalibur {
-    address public manaTokenAddress;
+contract Dexcalibur is ERC721Full {
+    address public king = 0x653036dDd25CeB5Ecc2D933f27e33D95C23F1043;
     uint256 public price = 0.01 ether;
-    bytes32 private solution = 0x4cc3df42c1d0dbd50175993afd98ef80447c81eb1387c9ef1b51751e31c8c63d;
-    bool public isGameOpen = true;
+    bool public kingHasBeenFound = false;
 
-    event LogSolutionFound(address indexed winner);
-    event LogAnotherPeasant(address indexed peasant);
-
-    constructor(address initialManaTokenAddress) public {
-        manaTokenAddress = initialManaTokenAddress;
+    constructor() public ERC721Full(
+        "Dexcalibur",
+        "DXCL"
+    ) {
     }
 
-    /**
-     * @dev Submits an answer
-     * @param x A parameter
-     * @param y Another parameter
-     * @param z Yet another parameter
-     * @return True if the solution is valid
-     */
-    function submitAnswer(
-        uint256 x,
-        uint256 y,
-        uint256 z
-    ) external payable returns (
-        bool
-    ) {
-        require(isGameOpen == true, "Game is already over");
-        require(msg.value == price, "Wrong price");
+    function amITheKing() external payable {
+        require(
+            kingHasBeenFound == false,
+            "The king has already been found"
+        );
 
-        IERC20 manaToken = IERC20(manaTokenAddress);
-        uint256 balance = manaToken.balanceOf(msg.sender);
+        require(
+            msg.value == price,
+            "The price is not right"
+        );
 
-        if (keccak256(abi.encodePacked(x, y, z, balance)) == solution) {
-            isGameOpen = false;
-            bool transfer = msg.sender.send(address(this).balance);
-            emit LogSolutionFound(msg.sender);
-            require(transfer == true, "Transfer failed");
+        require(
+            msg.sender == king,
+            "You are not the king"
+        );
 
-            return true;
-        } else {
-            emit LogAnotherPeasant(msg.sender);
-
-            return false;
-        }
+        kingHasBeenFound = true;
+        _mint(msg.sender, 0);
+        _setTokenURI(0, "https://ipfs.globalupload.io/QmVncbmEBg53YMytFdr2L4kvTsn5BmbkneNWskM1J1Bnvr");
     }
 }
