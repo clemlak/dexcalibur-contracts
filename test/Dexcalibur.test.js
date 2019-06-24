6f1603855a5f619a8213a31b990948b92edd2405
Clement Lakhal <clementlakhal@gmail.com>
2019-06-24 02:00:22 +0200

    Update tests

--- test/Dexcalibur.test.js
+++ test/Dexcalibur.test.js
@@ -1,35 +1,18 @@
 /* eslint-env node, mocha */
 /* global artifacts, contract, it, assert, web3 */
 
-const TestToken = artifacts.require('TestToken');
 const Dexcalibur = artifacts.require('Dexcalibur');
 
 let instance;
-let token;
 
 contract('Dexcalibur', (accounts) => {
-  it('Should deploy an instance of the TestToken contract', () => TestToken.deployed()
-    .then((contractInstance) => {
-      token = contractInstance;
-    }));
-
-  it('Should deploy an instance of the Dexcalibur contract', () => Dexcalibur.deployed(token.address)
+  it('Should deploy an instance of the Dexcalibur contract', () => Dexcalibur.deployed()
     .then((contractInstance) => {
       instance = contractInstance;
     }));
 
-  it('Should check the balance of account 0', () => token.balanceOf(accounts[0])
-    .then((balance) => {
-      assert.equal(balance.toString(), web3.utils.toWei('42'), 'Account 0 balance is wrong');
-    }));
-
-  it('Should try to submit an answer', () => instance.submitAnswer(0, 1, 2, {
+  it('Should check if account 0 is the king', () => instance.amITheKing({
     from: accounts[0],
     value: web3.utils.toWei('0.01'),
   }));
-
-  it('Should check if the game is still open', () => instance.isGameOpen()
-    .then((res) => {
-      assert.equal(res, false, 'Game should be closed');
-    }));
 });
